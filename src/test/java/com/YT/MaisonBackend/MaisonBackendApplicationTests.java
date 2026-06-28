df613a5cf06f13a111f8264f68ab63eb59929e1e
prince8250 <princeappiah8250@gmail.com>
2026-06-28 23:57:24 +0000

    Initial Commit

---
+++ src/test/java/com/YT/MaisonBackend/MaisonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YT.MaisonBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaisonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
